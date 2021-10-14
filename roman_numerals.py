c4b1f1427283eae895cd4d23c12842d4bac6d457
mstojkov <mstojkov@cisco.com>
2021-10-14 18:27:43 +0100

    convert 1 to I

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,2 +1,5 @@
 def func(x):
     return x + 2
+
+def convert_to_roman_numeral(x):
+    return 'I'
--- roman_numerals_test.py
+++ roman_numerals_test.py
@@ -1,4 +1,8 @@
 
 from roman_numerals import func
+from roman_numerals import convert_to_roman_numeral
 def test_answer():
     assert func(3) == 5
+
+def test_convert_one():
+    assert convert_to_roman_numeral(1) == 'I'
