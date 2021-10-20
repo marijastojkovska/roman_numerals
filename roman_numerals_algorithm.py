087364aac256b5d3232bc9a1b6269f62182fcda3
mstojkov <mstojkov@cisco.com>
2021-10-20 15:37:55 +0100

    variable naming clarification

--- roman_numerals_algorithm.py
+++ roman_numerals_algorithm.py
@@ -4,10 +4,10 @@ def convert_integer_to_roman(x):
     "40": "XL", "50": "L", "90": "XC", "100": "C", "400": "CD", "500": "D",
     "900": "CM", "1000": "M"}
 
-    dec_value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    integer_keys_reverse = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     temp = ''
     x = int(x)
-    for i in dec_value:
+    for i in integer_keys_reverse:
         if x != 0:
             if i <= x:
                 x = x- i
