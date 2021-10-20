80b027764c208eb99a6850290e58dc52e439ec29
mstojkov <mstojkov@cisco.com>
2021-10-20 15:35:33 +0100

    imrpovements to algorithm

--- roman_numeral_algorithm_test.py
+++ roman_numeral_algorithm_test.py
@@ -1,4 +1,4 @@
 from roman_numerals_algorithm import convert_integer_to_roman
 
-def test_convert_7():
-    assert convert_integer_to_roman(7) == 'VII'
+def test_convert_12():
+    assert convert_integer_to_roman(12) == 'XII'
--- roman_numerals_algorithm.py
+++ roman_numerals_algorithm.py
@@ -1,16 +1,21 @@
 
 def convert_integer_to_roman(x):
-    integer_to_roman = {'1': 'I', '4': 'IV', '5': 'V', '9': 'IX', '10': 'X',
-    '40': 'XL', '50': 'L', '90': 'XC', '100': 'C', '400': 'CD', '500': 'D',
-    '900': 'CM', '1000': 'M'}
+    integer_to_roman = {"1": "I", "4": "IV", "5": "V", "9": "IX", "10": "X",
+    "40": "XL", "50": "L", "90": "XC", "100": "C", "400": "CD", "500": "D",
+    "900": "CM", "1000": "M"}
 
     dec_value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     temp = ''
-
+    x = int(x)
     for i in dec_value:
         if x != 0:
             if i <= x:
                 x = x- i
-                temp = temp + str(integer_to_roman[i])
-        print(temp)
-        return temp
+                temp = temp + str(integer_to_roman.get(i))
+
+    return temp
+
+
+#num = input('Enter a number: ' )
+#result = convert_integer_to_roman(num)
+#print(result)
